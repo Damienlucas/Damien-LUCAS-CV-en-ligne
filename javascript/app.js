09764871a8ef41c5ca7953b8bb31b71208ab0644
Damienlucas <lucas.damien@free.fr>
2021-09-25 21:20:12 +0200

    feat: debut de mise en place CSS

--- javascript/app.js
+++ javascript/app.js
@@ -5,3 +5,12 @@ var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
   return new bootstrap.Offcanvas(offcanvasEl)
 })
 
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+
