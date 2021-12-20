d4d26f4a8ce267bdc03162e6db427393de8aeb73
Damienlucas <lucas.damien@free.fr>
2021-12-20 19:39:25 +0100

    feat: modif photo a propos + modif js timeline

--- javascript/app.js
+++ javascript/app.js
@@ -51,3 +51,22 @@ function isTweening(){
 }
 
 // ---------------------fin animation explosion emojis---------------
+// ----------------début animation timeline partie experiences---------------
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+const controller = new ScrollMagic.Controller()
+allBoxes.forEach(box => {
+    for(i = 0; i < allRonds.length; i++){
+        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
+            let scene = new ScrollMagic.Scene({
+                triggerElement: allRonds[i],
+                Reverse: false
+            })
+            .setTween(tween)
+            // .addIndicators()
+            .addTo(controller)
+        }
+    }
+})
+// ----------------fin animation timeline partie experiences---------------
